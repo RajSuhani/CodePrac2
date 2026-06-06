f38b1048ef8153a6f042e770e41ad9c3ae935ce2
RajSuhani <ak8045561@gmail.com>
2026-06-06 16:49:56 +0530

    Day 56: Left and Right Sum Differences

---
+++ practiceQ57.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> leftRightDifference(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> ans(n);
+        int totalSum = accumulate(nums.begin(), nums.end(), 0);
+
+        int leftSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            totalSum -= nums[i];
+            ans[i] = abs(leftSum - totalSum);
+            leftSum += nums[i];
+        }
+
+        return ans;
+    }
+};
