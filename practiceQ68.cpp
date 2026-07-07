3ef015abc0617e7b693b3e6978af57c58473f5f7
RajSuhani <ak8045561@gmail.com>
2026-07-07 15:03:14 +0530

    Day 67: Concatenate Non-Zero Digits and Multiply by Sum I

---
+++ practiceQ68.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long sumAndMultiply(int n) {
+        string s = to_string(n);
+
+        long long x = 0;
+        int sum = 0;
+
+        for (char c : s) {
+            if (c != '0') {
+                x = x * 10 + (c - '0');
+                sum += c - '0';
+            }
+        }
+
+        return x * 1LL * sum;
+    }
+};
