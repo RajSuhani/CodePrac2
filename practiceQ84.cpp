d6cf5062a366efdba943e7806aa17563a3eb1686
RajSuhani <ak8045561@gmail.com>
2026-07-23 16:02:03 +0530

    Day 83: Number of Unique XOR Triplets I

---
+++ practiceQ84.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        int n = nums.size();
+
+        if (n <= 2) {
+            return n;
+        }
+
+        int ans = 1;
+        while (ans <= n) {
+            ans <<= 1;
+        }
+
+        return ans;
+    }
+};
