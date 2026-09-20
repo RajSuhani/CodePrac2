a46887f96a2634c4fa6270c0e74336f4ddaf5421
RajSuhani <ak8045561@gmail.com>
2026-09-20 12:27:08 +0530

    Day 105 : Reverse Degree of a String

---
+++ practiceQ108.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int ans = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            int reverseValue = 'z' - s[i] + 1;
+            int position = i + 1;
+
+            ans += reverseValue * position;
+        }
+
+        return ans;
+    }
+};
